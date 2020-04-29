22db346f1543e3305d4c9900abfe41a733107665
iGxnon <igxnon@gmail.com>
2020-04-29 23:20:50 +0800

    增加了construct

--- src/xyz/lightsky/ModelManage/Bean/Bones.java
+++ src/xyz/lightsky/ModelManage/Bean/Bones.java
@@ -10,4 +10,14 @@ public class Bones {
     public String parent;
     public List<Integer> rotation;
     public List<Cubes> cubes;
+
+    public Bones(String name, List<Double> pivot, boolean mirror, boolean neverRender, String parent, List<Integer> rotation, List<Cubes> cubes) {
+        this.name = name;
+        this.pivot = pivot;
+        this.mirror = mirror;
+        this.neverRender = neverRender;
+        this.parent = parent;
+        this.rotation = rotation;
+        this.cubes = cubes;
+    }
 }
--- src/xyz/lightsky/ModelManage/Bean/Cubes.java
+++ src/xyz/lightsky/ModelManage/Bean/Cubes.java
@@ -6,4 +6,10 @@ public class Cubes{
     public List<Double> origin;
     public List<Double> size;
     public List<Integer> uv;
+
+    public Cubes(List<Double> origin, List<Double> size, List<Integer> uv) {
+        this.origin = origin;
+        this.size = size;
+        this.uv = uv;
+    }
 }
